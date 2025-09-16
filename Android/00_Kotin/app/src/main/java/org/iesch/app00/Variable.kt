393fd43dcf5b0163d828c7ad58139dff0c32d869
arodriguezgim <arodriguezg@iesch.org>
2025-09-16 21:20:23 +0200

    Primera clase Moviles

---
+++ Android/00_Kotin/app/src/main/java/org/iesch/app00/Variable.kt
@@ -0,0 +1,17 @@
+package org.iesch.app00
+
+// Variables
+val pi = 3.14159
+// numéricas
+var edad:Int = 35
+var edadGrande: Long = 4
+var po: Float = 3.14f // han de terminar con f y soporta hasta 6 decimales
+// alfanuméricas
+var letra: Char = 'a'
+var cadena: String = "Es un contenedor de dame lo que quieras"
+//Boolanas
+var esMartes: Boolean = true // Solo adminte valores true o false
+
+fun main() {
+
+}
