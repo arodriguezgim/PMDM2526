1d1c09a9f2c0ab6262e7ffb4703c48c8b970d1d4
arodriguezgim <arodriguezg@iesch.org>
2025-09-23 20:28:28 +0200

    Fin 2 hora

--- Android/01_Edad_Canina/app/src/main/java/org/iesch/edadcanina/MainActivity.kt
+++ Android/01_Edad_Canina/app/src/main/java/org/iesch/edadcanina/MainActivity.kt
@@ -1,12 +1,17 @@
 package org.iesch.edadcanina
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +22,30 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         // Aqui la pantalla está mostrada
+
+        // 1º - Tomamos el control de todos los elementos de la parte de la UI
+        val resultText = findViewById<TextView>(R.id.tvRespuesta)
+        val calculateButton = findViewById<Button>(R.id.btnCalcular)
+        val ageEdit = findViewById<EditText>(R.id.etEdad)
+        // 2 - Los botones tienen la propiedad setOnClickListener al pulsarlo
+        calculateButton.setOnClickListener {
+            // Aqui va el codigo de lo que queremos hacer al pulsar el boton de Calcular
+            val edadString = ageEdit.text.toString()
+
+            if (edadString.isEmpty()){
+                // No hacemos nada
+            } else {
+                val edadInt = edadString.toInt()
+
+                val dogAge = edadInt * 7
+
+                resultText.text = "Si fueras un perro, tu edad sería de $dogAge años."
+            }
+
+
+
+
+        }
 
     }
 }
