3f26593ece410f407a9aa5065d120bd2b3c17e33
arodriguezgim <arodriguezg@iesch.org>
2025-09-24 20:26:53 +0200

    Fin segunda clase

--- Android/02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_superheroes/detalle/DetalleHeroeActivity.kt
+++ Android/02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_superheroes/detalle/DetalleHeroeActivity.kt
@@ -1,16 +1,17 @@
-package org.iesch.a02_registro_superheroes
+package org.iesch.a02_registro_superheroes.detalle
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.iesch.a02_registro_superheroes.R
 
-class RegisterActivity : AppCompatActivity() {
+class DetalleHeroeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_detalle_heroe)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
