516283edd6ab4325c968f9d826b896759f814b57
arodriguezgim <arodriguezg@iesch.org>
2025-09-30 19:39:46 +0200

    Recibimos los datos del intent

--- Android/02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_superheroes/detalle/DetalleHeroeActivity.kt
+++ Android/02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_superheroes/detalle/DetalleHeroeActivity.kt
@@ -20,5 +20,30 @@ class DetalleHeroeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //  1 - Recibimos  los objetos del Intent
+        // Un objeto Bundle es un contenedor de datos que permite almacenar y transportar multiples valores en Activyties o Fragmentos
+        val bundle = intent.extras!!
+        val superHeroName = bundle.getString("heroName") ?: "No hay nombre"
+        val alterEgo = bundle.getString("alter_ego") ?: "No hay alter ego"
+        val bio = bundle.getString("bio") ?: "No hay bio"
+        val power = bundle.getFloat("power")
+        // 2 - Rellenamos con los campos que hemos recibido del intent
+        binding.tvHeroNameResult.text = superHeroName
+        binding.tvAlterEgoResult.text = alterEgo
+        binding.tvBioResult.text = bio
+        binding.rbResultado.rating = power
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
