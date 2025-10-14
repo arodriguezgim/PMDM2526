da88308731db5d9513cc6a1e37872b3ae3c1b2db
arodriguezgim <arodriguezg@iesch.org>
2025-10-14 19:48:33 +0200

    Fin cliclo de vida

--- Android/06_Ciclo_de_vida/app/src/main/java/org/iesch/a06_ciclo_de_vida/MainActivity.kt
+++ Android/06_Ciclo_de_vida/app/src/main/java/org/iesch/a06_ciclo_de_vida/MainActivity.kt
@@ -1,6 +1,7 @@
 package org.iesch.a06_ciclo_de_vida
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Log.i("ciclovida", "Entramos en el método onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("ciclovida", "Entramos en el método onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("ciclovida", "Entramos en el método onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("ciclovida", "Entramos en el método onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e("ciclovida", "Entramos en el método onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("ciclovida", "Entramos en el método onDestroy")
     }
 }
