2d3582f36aec1b4ad462f82946e10ef6e5edc5f6
arodriguezgim <arodriguezg@iesch.org>
2025-10-22 14:38:22 +0200

    Añadidos edad canina y superheroes

--- Android/03_Menu_Principal/app/src/main/java/org/iesch/a03_menu_principal/MenuActivity.kt
+++ Android/03_Menu_Principal/app/src/main/java/org/iesch/a03_menu_principal/MenuActivity.kt
@@ -8,7 +8,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import org.iesch.a03_menu_principal.apirazas.RazasApiActivity
 import org.iesch.a03_menu_principal.databinding.ActivityMenuBinding
+import org.iesch.a03_menu_principal.edadcanina.EdadCaninaActivity
 import org.iesch.a03_menu_principal.fragments.FragmentsActivity
+import org.iesch.a03_menu_principal.superheroes.RegistroSuperHeroeActivity
 
 class MenuActivity : AppCompatActivity() {
 
@@ -30,11 +32,26 @@ class MenuActivity : AppCompatActivity() {
         binding.btnFragments.setOnClickListener {
             irAMenuFragments()
         }
+        binding.btnEdadCanina.setOnClickListener {
+            irAEdadCanina()
+        }
+        binding.btnSuperheroes.setOnClickListener {
+            irASuperHeroes()
+        }
+
     }
 
     private fun irAMenuFragments() {
         val irAFragments = Intent(this, FragmentsActivity::class.java)
         startActivity(irAFragments)
+    }
+    private fun irAEdadCanina() {
+        val irAEdadCanina = Intent(this, EdadCaninaActivity::class.java)
+        startActivity(irAEdadCanina)
+    }
+    private fun irASuperHeroes() {
+        val irASuperHeroes = Intent(this, RegistroSuperHeroeActivity::class.java)
+        startActivity(irASuperHeroes)
     }
 }
 
