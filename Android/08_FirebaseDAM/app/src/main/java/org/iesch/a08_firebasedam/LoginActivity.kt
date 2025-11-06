75ed58ab30c18fb441350af02f15e24e23006a94
arodriguezgim <arodriguezg@iesch.org>
2025-11-06 19:00:13 +0100

    Reorganizacion de codigo

--- Android/08_FirebaseDAM/app/src/main/java/org/iesch/a08_firebasedam/LoginActivity.kt
+++ Android/08_FirebaseDAM/app/src/main/java/org/iesch/a08_firebasedam/LoginActivity.kt
@@ -35,11 +35,10 @@ import org.iesch.a08_firebasedam.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-
     private lateinit var firebaseAnalytics: FirebaseAnalytics
     private lateinit var auth: FirebaseAuth
 
-    //Creamos ellauncher para solicitar los permisos
+    //Launcher para solicitar permisos
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission(),
     ) { isGranted: Boolean ->
@@ -59,78 +58,81 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        // Analytics
         iniciarAnalytics()
         // Solicitar PErmisos de Notificaciones
         solicitarPermisosPush()
         // Notificaciones Push
         notificacionesPush()
-        // 2 - Iniciamosla instancia de Firebase auth
-        // Initialize Firebase Auth
-        auth = Firebase.auth
 
-        // Me puedo suscribir por temas
-        Firebase.messaging.subscribeToTopic("RealMadrid")
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful ){
-                    Log.d("FCM", "Suscrito a Real MAdrid")
-                } else {
-                    Log.d("FCM", "Error en la suscripcion a Real MAdrid")
-                }
-            }
+        // Iniciamos los listeners para los botones
+        initUI()
+
+    }
+
+    private fun initUI() {
 
         // Configuramos los listeners de los botones
         binding.loginButton.setOnClickListener {
-            // Comprobamos si hemos introducido email y contraseña
-            if ( binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty() ){
-                // Nos autenticamos con email y contraseña
-                val usuario = binding.emailEditText.text.toString()
-                val password = binding.passwordEditText.text.toString()
-                auth.signInWithEmailAndPassword( usuario, password )
-                // Añadimos un listener para comprobar si el usuario se ha logueado correctamente o no
-                    .addOnCompleteListener { logueo ->
-                        if ( logueo.isSuccessful ){
-                            // El usuario se ha logueado correctamente
-                            mostrarHomeActivity( usuario, ProviderType.EMAILYCONTRASENA.toString() )
-                        } else {
-                            // Ha habido un error
-                            mostrarError()
-                        }
-                    }
-
-            } else {
-                // Avisamos al usuario que ha de rellenar los campos
-                avisoUsuario()
-            }
+            logueoConUsuarioYContrasena()
         }
-
         binding.registerButton.setOnClickListener {
-            // Comprobamos si hemos introducido email y contraseña
-            if ( binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty() ){
-                // Nos autenticamos con email y contraseña
-                val usuario = binding.emailEditText.text.toString()
-                val password = binding.passwordEditText.text.toString()
-                auth.createUserWithEmailAndPassword( usuario, password )
-                    // Añadimos un listener para comprobar si el usuario se ha registrado correctamente o no
-                    .addOnCompleteListener { registro ->
-                        if ( registro.isSuccessful ){
-                            // El usuario se ha registrado correctamente
-                            mostrarRegistroCorrecto()
-                        } else {
-                            // Ha habido un error
-                            mostrarError()
-                        }
-                    }
-
-            } else {
-                // Avisamos al usuario que ha de rellenar los campos
-                avisoUsuario()
-            }
+            registroConUsuarioYContrasena()
         }
 
         binding.loginGoogleButton.setOnClickListener {
             logueoConGoogle()
         }
 
+    }
+
+    private fun logueoConUsuarioYContrasena() {
+        // Comprobamos si hemos introducido email y contraseña
+        if ( binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty() ){
+            // Nos autenticamos con email y contraseña
+            val usuario = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            auth.signInWithEmailAndPassword( usuario, password )
+                // Añadimos un listener para comprobar si el usuario se ha logueado correctamente o no
+                .addOnCompleteListener { logueo ->
+                    if ( logueo.isSuccessful ){
+                        // El usuario se ha logueado correctamente
+                        mostrarHomeActivity( usuario, ProviderType.EMAILYCONTRASENA.toString() )
+                    } else {
+                        // Ha habido un error
+                        mostrarError()
+                    }
+                }
+        } else {
+            // Avisamos al usuario que ha de rellenar los campos
+            avisoUsuario()
+        }
+    }
+
+    private fun registroConUsuarioYContrasena() {
+        // Comprobamos si hemos introducido email y contraseña
+        if ( binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty() ){
+            // Nos autenticamos con email y contraseña
+            val usuario = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            auth.createUserWithEmailAndPassword( usuario, password )
+                // Añadimos un listener para comprobar si el usuario se ha registrado correctamente o no
+                .addOnCompleteListener { registro ->
+                    if ( registro.isSuccessful ){
+                        // El usuario se ha registrado correctamente
+                        mostrarRegistroCorrecto()
+                    } else {
+                        // Ha habido un error
+                        mostrarError()
+                    }
+                }
+
+        } else {
+            // Avisamos al usuario que ha de rellenar los campos
+            avisoUsuario()
+        }
     }
 
     private fun solicitarPermisosPush() {
@@ -162,6 +164,16 @@ class LoginActivity : AppCompatActivity() {
                 Log.d("FCM", "Error al obtener el token de registro")
             }
         }
+
+        // Me puedo suscribir por temas
+        Firebase.messaging.subscribeToTopic("RealMadrid")
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful ){
+                    Log.d("FCM", "Suscrito a Real Madrid")
+                } else {
+                    Log.d("FCM", "Error en la suscripcion a Real Madrid")
+                }
+            }
     }
 
     private fun logueoConGoogle() {
