c2fcaa15ce029ff25ea08e1f370191eac5b7c983
arodriguezgim <arodriguezg@iesch.org>
2025-11-19 21:21:53 +0100

    Fin clase Miercoles

---
+++ Flutter/contador/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:contador/screens/counter_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp() );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color.fromARGB(255, 25, 158, 129)
+      ),
+      home: CounterScreen()
+    );
+  }
+}
---
+++ Flutter/contador/lib/screens/counter_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+
+  int contador = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter Screen'),
+        centerTitle: true,
+      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(' $contador ', style: TextStyle( fontSize: 160, fontWeight:FontWeight.w100 ),),
+              Text('Clicks', style: TextStyle(fontSize: 25),),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon( Icons.add),
+            onPressed: (){
+              setState(() {
+                contador++;
+              });
+              
+            }
+          ),
+      );
+  }
+}
