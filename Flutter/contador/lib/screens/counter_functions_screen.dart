4f9f1d7246db4ff1ad1c79f87d31399ef6d44409
arodriguezgim <arodriguezg@iesch.org>
2025-11-25 20:05:13 +0100

    Fin App contador

--- Flutter/contador/lib/screens/counter_functions_screen.dart
+++ Flutter/contador/lib/screens/counter_functions_screen.dart
@@ -15,26 +15,52 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        //leading: IconButton(onPressed: (){}, icon: Icon(Icons.ac_unit)),
         title: Text('Counter Functions Screen'),
         centerTitle: true,
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.settings)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.ac_unit)),
+        ],
       ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(' $contador ', style: TextStyle( fontSize: 160, fontWeight:FontWeight.w100 ),),
-              Text('Clicks', style: TextStyle(fontSize: 25),),
+              Text('Click${ contador == 1 ? '' : 's'}', style: TextStyle(fontSize: 25),),
             ],
           ),
         ),
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            BotonPersonalizado(),
+            BotonPersonalizado( 
+              icono: Icons.refresh_rounded ,
+              onPressed: () {
+                setState(() {
+                  contador = 0;
+                });
+              },
+              ),
             SizedBox( height: 10,),
-            BotonPersonalizado(),
+            BotonPersonalizado( 
+              icono: Icons.plus_one,
+              onPressed: () {
+                setState(() {
+                  contador++;
+                });
+              },),
             SizedBox( height: 10,),
-            BotonPersonalizado(),
+            BotonPersonalizado( 
+              icono: Icons.exposure_minus_1,
+              onPressed: () {
+                setState(() {
+                  if ( contador == 0 ) return; 
+                  contador --;
+                });
+              },),
+            
           ],
         ),
       );
@@ -42,14 +68,18 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
 }
 
 class BotonPersonalizado extends StatelessWidget {
+
+  final IconData icono;
+  final VoidCallback? onPressed;
+
+  const BotonPersonalizado({super.key, required this.icono, this.onPressed});
   
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      child: Icon( Icons.add),
-        onPressed: (){
-          
-        }
+      //backgroundColor: Colors.yellow,
+      child: Icon( icono ),
+        onPressed: onPressed
       );
   }
 }
