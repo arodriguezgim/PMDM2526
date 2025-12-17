205d1c76be1eb1960e38bfc915f629d3a11a206b
arodriguezgim <arodriguezg@iesch.org>
2025-12-17 20:26:38 +0100

    Fin case 2

---
+++ Flutter/superhero_app/lib/data/model/superhero_detail_response.dart
@@ -0,0 +1,15 @@
+class SuperheroDetailResponse {
+
+  final String id;
+  final String name;
+
+  SuperheroDetailResponse({required this.id, required this.name});
+
+  factory SuperheroDetailResponse.fromJson( Map<String, dynamic> json ){
+    return SuperheroDetailResponse(
+      id: json['id'],
+      name: json['name']
+    );
+  }
+  
+}
--- Flutter/superhero_app/lib/data/model/superhero_response.dart
+++ Flutter/superhero_app/lib/data/model/superhero_response.dart
@@ -1,14 +1,28 @@
+import 'package:superhero_app/data/model/superhero_detail_response.dart';
+
 class SuperHeroResponse {
 
   final String response;
+  final List<SuperheroDetailResponse> listaSuperHeroes;
 
-  SuperHeroResponse({required this.response});
+  SuperHeroResponse({
+    required this.response, 
+    required this.listaSuperHeroes,
+    });
 
   // Necesito devolver un objeto SuperHerpResponse con los campos que me interesan
   factory SuperHeroResponse.fromJson( Map<String, dynamic> json ){
+    var lista = json['results'] as List;
+
+    // El.map escomo un for que recorre la lista y la aplica a la funcion que pasamos
+    List<SuperheroDetailResponse> listaSuperHeroes = lista
+        .map( (heroe)=> SuperheroDetailResponse.fromJson( heroe))
+        .toList();
+    final response = json['response'];
     return SuperHeroResponse(
-      response: json['response']
-    );
+      response: response, 
+      listaSuperHeroes: listaSuperHeroes,
+      );
   }
 
 }
