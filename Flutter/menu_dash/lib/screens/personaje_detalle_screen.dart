62ebac6c40c49a5130763326426a408b8dbabe89
arodriguezgim <arodriguezg@iesch.org>
2026-01-21 19:13:06 +0100

    Fin Estilos

--- Flutter/menu_dash/lib/screens/personaje_detalle_screen.dart
+++ Flutter/menu_dash/lib/screens/personaje_detalle_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:menu_dash/widgets/blur_container.dart';
+import 'package:menu_dash/widgets/fade_animation_widget.dart';
 import 'package:menu_dash/widgets/infotitle_widget.dart';
 
 class PersonajeDetalleScreen extends StatefulWidget {
@@ -39,31 +40,32 @@ class _PersonajeDetalleScreenState extends State<PersonajeDetalleScreen> {
           children: [
             Stack(
               children: [
-                Container(
-                  child: SizedBox(
-                    height: _alturaPantalla * 0.6,
-                    child: Hero(
-                      tag: widget.imagen,
-                      child: Image.asset("assets/${widget.imagen}.png")),
-                  ),
+                SizedBox(
+                  height: _alturaPantalla * 0.6,
+                  child: Hero(
+                    tag: widget.imagen,
+                    child: Image.asset("assets/${widget.imagen}.png")),
                 ),
                 Positioned(
                   bottom: 20,
                   left: 12,
-                  child: BlurContainer(
-                    child: Container(
-                      width: 160,
-                      height: 50,
-                      alignment: .center,
-                      decoration: BoxDecoration(
-                        color: Colors.white.withValues(alpha: 0.1),
-                        borderRadius: .circular(10),
-                      ),
-                      child: Text(
-                        widget.nombre,
-                        style: TextStyle(
-                          fontWeight: .bold,
-                          color: Colors.white,
+                  child: FadeAnimation(
+                    intervalEnd: .8,
+                    child: BlurContainer(
+                      child: Container(
+                        width: 160,
+                        height: 50,
+                        alignment: .center,
+                        decoration: BoxDecoration(
+                          color: Colors.white.withValues(alpha: 0.1),
+                          borderRadius: .circular(10),
+                        ),
+                        child: Text(
+                          widget.nombre,
+                          style: TextStyle(
+                            fontWeight: .bold,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
@@ -74,46 +76,58 @@ class _PersonajeDetalleScreenState extends State<PersonajeDetalleScreen> {
             SizedBox(height: 10),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: Text(
-                "Personaje: ${widget.nombre}",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: .bold,
+              child: FadeAnimation(
+                intervalStart: .3,
+                child: Text(
+                  "Personaje: ${widget.nombre}",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: .bold,
+                  ),
                 ),
               ),
             ),
             SizedBox(height: 5),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: Text(
-                "One Piece",
-                style: TextStyle(color: Colors.white70, fontSize: 12),
+              child: FadeAnimation(
+                intervalStart: .4,
+                child: Text(
+                  "One Piece",
+                  style: TextStyle(color: Colors.white70, fontSize: 12),
+                ),
               ),
             ),
             SizedBox(height: 50),
             Padding(
               padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
-              child: Row(
-                mainAxisAlignment: .spaceBetween,
-                children: [
-                  InfoTitleWidget(titulo: "Eiichiró Oda", subtitulo: "Creador"),
-                  InfoTitleWidget(titulo: "Japón", subtitulo: "País"),
-                ],
+              child: FadeAnimation(
+                intervalStart: .5,
+                child: Row(
+                  mainAxisAlignment: .spaceBetween,
+                  children: [
+                    InfoTitleWidget(titulo: "Eiichiró Oda", subtitulo: "Creador"),
+                    InfoTitleWidget(titulo: "Japón", subtitulo: "País"),
+                  ],
+                ),
               ),
             ),
             SizedBox(height: 50,),
             GestureDetector(
               onTap: () => Navigator.pop(context),
-              child: Container(
-                height: 50,
-                alignment: .center,
-                margin: EdgeInsets.symmetric(horizontal: 12),
-                decoration: BoxDecoration(
-                  color: widget.color,
-                  borderRadius: .circular(10)
+              child: FadeAnimation(
+                intervalStart: .6,
+                child: Container(
+                  height: 50,
+                  alignment: .center,
+                  margin: EdgeInsets.symmetric(horizontal: 12),
+                  decoration: BoxDecoration(
+                    color: widget.color,
+                    borderRadius: .circular(10)
+                  ),
+                  child: Text("Volver", style: TextStyle(color: Colors.white60, fontSize: 16, fontWeight: .bold),),
                 ),
-                child: Text("Volver", style: TextStyle(color: Colors.white60, fontSize: 16, fontWeight: .bold),),
               ),
             )
           ],
